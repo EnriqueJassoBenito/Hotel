81a8391b56b82faf1884883f72d84b9cc46ea0b4
EnriqueJassoBenito <20213tn033@utez.edu.mx>
2025-12-10 23:42:32 -0600

    [FIX] Mas arreglos...

--- FrontHotel/js/auth.js
+++ FrontHotel/js/auth.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8081';
+const API_BASE_URL = 'http://localhost:8082';
 // const API_BASE_URL = 'http://44.216.242.226:8080';
 
 // Si usamos ngrok, agregamos el header especial
